33a0abdb30f67d1875570ff62e08cb948770a0b9
mariajose <mariajosemonterotroncos@gmail.com>
2022-11-22 13:01:03 -0500

    permisos-roles

---
+++ src/controllers/permisos_roles.js
@@ -0,0 +1,65 @@
+const { connection } = require("../db");
+
+//select jugadores
+function index(req, res) {
+  // with placeholder
+  connection.query(` select * from permisos_roles pr`, function (err, results) {
+    res.send(results);
+  });
+}
+
+//creando un jugadores
+function store(req, res) {
+  const data = req.body;
+  const id_permisos = data.id_permisos;
+  const id_roles = data.id_roles;
+
+  connection.query(
+    `insert into equipos(
+      id_permisos,
+      id_roles)
+    values (?)`,
+    [[id_permisos, id_roles]],
+    (error, results) => {
+      res.send(results);
+    }
+  );
+}
+
+//actualizando juadores
+function update(req, res) {
+  const id = req.params.id;
+  //const { nombre, nacionalidad, sejuego, nombre_torneos, edad, sexo } = req.body;
+
+  const id_permisos = req.body.id_permisos;
+  const id_roles = req.body.id_roles;
+
+  connection.query(
+    `update permisos_roles SET id_permisos=?,id_roles=? where id=?;`,
+    [id_permisos, id_roles, id],
+
+    (error, results) => {
+      res.send(results);
+      console.log(error);
+    }
+  );
+}
+
+//eliminando un jugador
+function destroy(req, res) {
+  const id = req.params.id;
+
+  connection.query(
+    ` delete from permisos_roles where id=${id}`,
+    (error, results) => {
+      res.send(results);
+    }
+  );
+}
+
+module.exports.permisos_rolesController = {
+  index,
+  store,
+  update,
+  destroy,
+};
--- src/rootes.js
+++ src/rootes.js
@@ -2,30 +2,34 @@ const express = require("express");
 const app = express();
 const { usuariosController } = require("./controllers/usuarios");
 const { jugadoresController } = require("./controllers/jugadores");
-const {arbitrosController} = require("./controllers/arbitros");
-const {equiposController} = require("./controllers/equipos");
-const {comportamientosController} = require("./controllers/comportamientos");
-const {estadiosController} = require("./controllers/estadios");
-const {entrenadoresController}  = require("./controllers/entrenadores");
-const {eventosController}= require("./controllers/eventos");
-const {faltasController} =require("./controllers/faltas");
-const {historial_partidasController} = require("./controllers/historial_partidas");
-const{juecesController} =require("./controllers/jueces");
-const {partidasController}=require("./controllers/partidas");
-const {partidas_jugadoresController}=require("./controllers/partidas_jugadores");
-const {rolesController}=require("./controllers/roles");
-const {rondasController}=require("./controllers/rondas");
-const {permisosController}=require("./controllers/permisos");
-const {perfilesController}=require("./controllers/perfiles");
-const {sancionesController}=require("./controllers/sanciones");
-const {torneosController}=require("./controllers/torneos");
-const {premiosController}=require("./controllers/premios");
-const {proximosencuentrosController}=require("./controllers/partidas");
+const { arbitrosController } = require("./controllers/arbitros");
+const { equiposController } = require("./controllers/equipos");
+const { comportamientosController } = require("./controllers/comportamientos");
+const { estadiosController } = require("./controllers/estadios");
+const { entrenadoresController } = require("./controllers/entrenadores");
+const { eventosController } = require("./controllers/eventos");
+const { faltasController } = require("./controllers/faltas");
+const {
+  historial_partidasController,
+} = require("./controllers/historial_partidas");
+const { juecesController } = require("./controllers/jueces");
+const { partidasController } = require("./controllers/partidas");
+const {
+  partidas_jugadoresController,
+} = require("./controllers/partidas_jugadores");
+const { rolesController } = require("./controllers/roles");
+const { rondasController } = require("./controllers/rondas");
+const { permisosController } = require("./controllers/permisos");
+const { perfilesController } = require("./controllers/perfiles");
+const { sancionesController } = require("./controllers/sanciones");
+const { torneosController } = require("./controllers/torneos");
+const { premiosController } = require("./controllers/premios");
+const { proximosencuentrosController } = require("./controllers/partidas");
 
-const {ciudadesController}=require("./controllers/ciudades");
-const {paisesController}=require("./controllers/paises");
+const { permisos_rolesController } = require("./controllers/permisos_roles");
 
-
+const { ciudadesController } = require("./controllers/ciudades");
+const { paisesController } = require("./controllers/paises");
 
 const controllers = require("./controllers");
 
@@ -42,7 +46,6 @@ app.put("/jugadores/:id", jugadoresController.update);
 
 //eliminando un jugador
 app.delete("/jugadores/:id", jugadoresController.destroy);
-
 
 // crud usuaris
 
@@ -66,13 +69,12 @@ app.put("/usuarios/:id", usuariosController.update);
 //eliminando un user
 app.delete("/usuarios/:id", usuariosController.destroy);
 
-
 //crud de roles/user
 
 app.get("/roles", rolesController.index);
 
 //creando un jugadores
-app.post("/roles",rolesController.store);
+app.post("/roles", rolesController.store);
 
 //actualizando juadores
 app.put("/roles/:id", rolesController.update);
@@ -80,15 +82,12 @@ app.put("/roles/:id", rolesController.update);
 //eliminando un jugador
 app.delete("/roles/:id", rolesController.destroy);
 
-
-
 //Crud permisos
-
 
 app.get("/permisos", permisosController.index);
 
 //creando un permi
-app.post("/permisos",permisosController.store);
+app.post("/permisos", permisosController.store);
 
 //actualizando permi
 app.put("/permisos/:id", permisosController.update);
@@ -96,11 +95,9 @@ app.put("/permisos/:id", permisosController.update);
 //eliminando un permi
 app.delete("/permisos/:id", permisosController.destroy);
 
-
 //Crud perfiles
 
-
-app.get("/perfiles",perfilesController.index);
+app.get("/perfiles", perfilesController.index);
 
 //creando un permi
 app.post("/perfiles", perfilesController.store);
@@ -111,7 +108,18 @@ app.put("/perfiles/:id", perfilesController.update);
 //eliminando un permi
 app.delete("/perfiles/:id", perfilesController.destroy);
 
+/// crud permisos-roles
 
+app.get("/permisos_roles", permisos_rolesController.index);
+
+//creando un jugadores
+app.post("/permisos_roles", permisos_rolesController.store);
+
+//actualizando juadores
+app.put("/permisos_roles/:id", permisos_rolesController.update);
+
+//eliminando un jugador
+app.delete("/permisos_roles/:id", permisos_rolesController.destroy);
 
 // crud de arbitros
 
@@ -127,14 +135,12 @@ app.put("/arbitros/:id", arbitrosController.update);
 //eliminando un arbit
 app.delete("/arbitros/:id", arbitrosController.destroy);
 
-
-
 // crud comportamientos
 
 app.get("/comportamientos", comportamientosController.index);
 
 //creando un compor
-app.post("/comportamientos",comportamientosController.store);
+app.post("/comportamientos", comportamientosController.store);
 
 //actualizando compor
 app.put("/comportamientos/:id", comportamientosController.update);
@@ -142,14 +148,12 @@ app.put("/comportamientos/:id", comportamientosController.update);
 //eliminando un compor
 app.delete("/comportamientos/:id", comportamientosController.destroy);
 
-
-
 // crud entrenadores
 
 app.get("/entrenadores", entrenadoresController.index);
 
 //creando un jugadores0
-app.post("/entrenadores",entrenadoresController.store);
+app.post("/entrenadores", entrenadoresController.store);
 
 //actualizando juadores
 app.put("/entrenadores/:id", entrenadoresController.update);
@@ -157,13 +161,12 @@ app.put("/entrenadores/:id", entrenadoresController.update);
 //eliminando un jugador
 app.delete("/entrenadores/:id", entrenadoresController.destroy);
 
-
 // crud equipos
 
 app.get("/equipos", equiposController.index);
 
 //creando un jugadores
-app.post("/equipos",equiposController.store);
+app.post("/equipos", equiposController.store);
 
 //actualizando juadores
 app.put("/equipos/:id", equiposController.update);
@@ -171,15 +174,12 @@ app.put("/equipos/:id", equiposController.update);
 //eliminando un jugador
 app.delete("/equipos/:id", equiposController.destroy);
 
-
-
 //crud estadios
-
 
 app.get("/estadios", estadiosController.index);
 
 //creando un jugadores
-app.post("/estadios",estadiosController.store);
+app.post("/estadios", estadiosController.store);
 
 //actualizando juadores
 app.put("/estadios/:id", estadiosController.update);
@@ -187,14 +187,12 @@ app.put("/estadios/:id", estadiosController.update);
 //eliminando un jugador
 app.delete("/estadios/:id", estadiosController.destroy);
 
-
 //evento
-
 
 app.get("/eventos", eventosController.index);
 
 //creando un jugadores
-app.post("/eventos",eventosController.store);
+app.post("/eventos", eventosController.store);
 
 //actualizando juadores
 app.put("/eventos/:id", eventosController.update);
@@ -202,14 +200,12 @@ app.put("/eventos/:id", eventosController.update);
 //eliminando un jugador
 app.delete("/eventos/:id", eventosController.destroy);
 
-
 //faltas
-
 
 app.get("/faltas", faltasController.index);
 
 //creando un jugadores
-app.post("/faltas",faltasController.store);
+app.post("/faltas", faltasController.store);
 
 //actualizando juadores
 app.put("/faltas/:id", faltasController.update);
@@ -217,15 +213,12 @@ app.put("/faltas/:id", faltasController.update);
 //eliminando un jugador
 app.delete("/faltas/:id", faltasController.destroy);
 
-
-
 //hisotrial patidas
-
 
 app.get("/historial_partidas", historial_partidasController.index);
 
 //creando un jugadores
-app.post("/historial_partidas",historial_partidasController.store);
+app.post("/historial_partidas", historial_partidasController.store);
 
 //actualizando juadores
 app.put("/historial_partidas/:id", historial_partidasController.update);
@@ -233,15 +226,12 @@ app.put("/historial_partidas/:id", historial_partidasController.update);
 //eliminando un jugador
 app.delete("/historial_partidas/:id", historial_partidasController.destroy);
 
-
 //Crud jueces
-
-
 
 app.get("/jueces", juecesController.index);
 
 //creando un jugadores
-app.post("/jueces",juecesController.store);
+app.post("/jueces", juecesController.store);
 
 //actualizando juadores
 app.put("/jueces/:id", juecesController.update);
@@ -256,7 +246,7 @@ app.get("/partidas", partidasController.index);
 //app.get("/proximosencuentros", proximosencuentrosController.index);
 
 //creando un jugadores
-app.post("/partidas",partidasController.store);
+app.post("/partidas", partidasController.store);
 
 //actualizando juadores
 app.put("/partidas/:id", partidasController.update);
@@ -264,13 +254,12 @@ app.put("/partidas/:id", partidasController.update);
 //eliminando un jugador
 app.delete("/partidas/:id", partidasController.destroy);
 
-
 //Crud Partidas_jugadores
 
 app.get("/partidas_jugadores", partidas_jugadoresController.index);
 
 //creando un jugadores
-app.post("/partidas_jugadores",partidas_jugadoresController.store);
+app.post("/partidas_jugadores", partidas_jugadoresController.store);
 
 //actualizando juadores
 app.put("/partidas_jugadores/:id", partidas_jugadoresController.update);
@@ -280,11 +269,10 @@ app.delete("/partidas_jugadores/:id", partidas_jugadoresController.destroy);
 
 //crud de rondas
 
-
 app.get("/rondas", rondasController.index);
 
 //creando un jugadores
-app.post("/rondas",rondasController.store);
+app.post("/rondas", rondasController.store);
 
 //actualizando juadores
 app.put("/rondas/:id", rondasController.update);
@@ -292,14 +280,12 @@ app.put("/rondas/:id", rondasController.update);
 //eliminando un jugador
 app.delete("/rondas/:id", rondasController.destroy);
 
-
 //crud de sanciones
-
 
 app.get("/sanciones", sancionesController.index);
 
 //creando un jugadores
-app.post("/sanciones",sancionesController.store);
+app.post("/sanciones", sancionesController.store);
 
 //actualizando juadores
 app.put("/sanciones/:id", sancionesController.update);
@@ -307,15 +293,12 @@ app.put("/sanciones/:id", sancionesController.update);
 //eliminando un jugador
 app.delete("/sanciones/:id", sancionesController.destroy);
 
-
-
 //crud de torneos
-
 
 app.get("/torneos", torneosController.index);
 
 //creando un jugadores
-app.post("/torneos",torneosController.store);
+app.post("/torneos", torneosController.store);
 
 //actualizando juadores
 app.put("/torneos/:id", torneosController.update);
@@ -323,14 +306,12 @@ app.put("/torneos/:id", torneosController.update);
 //eliminando un jugador
 app.delete("/torneos/:id", torneosController.destroy);
 
-
 //crud de premios
-
 
 app.get("/premios", premiosController.index);
 
 //creando un jugadores
-app.post("/premios",premiosController.store);
+app.post("/premios", premiosController.store);
 
 //actualizando juadores
 app.put("/premios/:id", premiosController.update);
@@ -338,16 +319,12 @@ app.put("/premios/:id", premiosController.update);
 //eliminando un jugador
 app.delete("/premios/:id", premiosController.destroy);
 
-
-
-
 //crud de paises
-
 
 app.get("/paises", paisesController.index);
 
 //creando un jugadores
-app.post("/paises",paisesController.store);
+app.post("/paises", paisesController.store);
 
 //actualizando juadores
 app.put("/paises/:id", paisesController.update);
@@ -355,24 +332,19 @@ app.put("/paises/:id", paisesController.update);
 //eliminando un jugador
 app.delete("/paises/:id", paisesController.destroy);
 
-
 //crud de ciudades
 
-
-app.get("/ciudades",ciudadesController.index);
+app.get("/ciudades", ciudadesController.index);
 
 //creando un jugadores
-app.post("/ciudades",ciudadesController.store);
+app.post("/ciudades", ciudadesController.store);
 
 //actualizando juadores
-app.put("/ciudades/:id",ciudadesController.update);
+app.put("/ciudades/:id", ciudadesController.update);
 
 //eliminando un jugador
-app.delete("/ciudades/:id",ciudadesController.destroy);
+app.delete("/ciudades/:id", ciudadesController.destroy);
 
 //crud de provincia
 
-
 module.exports = app;
-
-
